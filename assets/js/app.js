6b5a7902a1bbac06723560c0ab2492a38f9ebae4
Williamfshaw <williamfshaw@icloud.com>
2016-10-27 18:29:13 -0400

    add event handlers, media query for iphone 6

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,6 @@
 window.onload = () => {
   getGames();
+
   function getGames() {
     const baseUrl = 'http://gdx.mlb.com/components/game/mlb/year_2016/month_09/day_20/master_scoreboard.json'
     fetch(baseUrl)
@@ -12,4 +13,28 @@ window.onload = () => {
       })
       .catch(err => err);
   }
+
+  function getElements() {
+    const elements = document.querySelectorAll('.carousel__thumbnails__content');
+    return {
+      elements,
+      first: elements[0],
+      last: elements[elements.length -1],
+      parent: elements[0].parentNode
+    }
+  }
+
+  document.querySelectorAll('.button').forEach((button) => {
+    button.addEventListener('click', (e) => {
+      console.log('clicked')
+      const { elements, first, last, parent } = getElements();
+      if(e.target.classList.contains('prev')) {
+        parent.removeChild(first);
+        parent.appendChild(first);
+      } else if(e.target.classList.contains('next')) {
+        parent.removeChild(last);
+        parent.insertBefore(last, first);
+      }
+    })
+  })
 }
