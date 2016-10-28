dc9c98939e7c21c4b69ce5cbccee853ace16ebf4
Williamfshaw <williamfshaw@icloud.com>
2016-10-27 20:57:42 -0400

    refactor, add local storage cache, add mouseover and mouseleave events to change image

---
+++ assets/js/CarouselThumbnailView.js
@@ -0,0 +1,52 @@
+class CarouselThumbnailView {
+  constructor(data, el) {
+    this.el = el;
+    this.headline = data.game_media.media[1].headline;
+    this.small_thumbnail = data.video_thumbnails.thumbnail[0].content;
+    this.large_thumbnail = data.video_thumbnails.thumbnail[1].content;
+    this.venue = data.venue;
+    this.render();
+  }
+
+  render() {
+    const thumbnailContainer = document.createElement('div');
+    thumbnailContainer.className = 'carousel__thumbnails__content';
+    const headline = this.createTextNode(this.headline);
+    thumbnailContainer.appendChild(headline);
+    const img = this.createImageNode(this.small_thumbnail);
+    thumbnailContainer.appendChild(img);
+    const venue = this.createTextNode(this.venue);
+    thumbnailContainer.appendChild(venue);
+    this.addMouseEnterEvent(img);
+    this.addMouseOutEvent(img);
+    this.el.appendChild(thumbnailContainer);
+  }
+
+  createTextNode(text) {
+    const div = document.createElement('div');
+    div.className = 'carousel__thumbnails__content__metainfo';
+    div.innerHTML = text;
+    return div;
+  }
+
+  createImageNode(img_url) {
+    const div = document.createElement('div');
+    div.className = 'carousel__thumbnails__content__img';
+    div.style.backgroundImage = `url(${img_url})`;
+    div.style.backgroundRepeat = 'no-repeat';
+    div.style.backgroundSize = 'cover';
+    return div;
+  }
+
+  addMouseEnterEvent(node) {
+    node.addEventListener('mouseover', (e) => {
+      e.target.style.backgroudImage = `url(${this.large_thumbnail})`;
+    })
+  }
+
+  addMouseOutEvent(node) {
+     node.addEventListener('mouseleave', (e) => {
+      e.target.style.backgroudImage = `url(${this.small_thumbnail})`;;
+    })
+  }
+}
--- assets/js/app.js
+++ assets/js/app.js
@@ -2,16 +2,24 @@ window.onload = () => {
   getGames();
 
   function getGames() {
-    const baseUrl = 'http://gdx.mlb.com/components/game/mlb/year_2016/month_09/day_20/master_scoreboard.json'
+    const gamesArray = localStorage.getItem('gamesArray');
+    if(gamesArray) {
+      JSON.parse(gamesArray).forEach(createThumbnail)
+      return;
+    }
+    const baseUrl = 'http://gdx.mlb.com/components/game/mlb/year_2016/month_09/day_20/master_scoreboard.json';
     fetch(baseUrl)
       .then((response) => {
         response.json().then((json) => {
-          json.data.games.game.forEach((game) => {
-            new CarosuelThumbnailView(game, document.querySelector('.carousel__thumbnails'));
-          });
+          localStorage.setItem("gamesArray", JSON.stringify(json.data.games.game))
+          json.data.games.game.forEach(createThumbnail);
         });
       })
       .catch(err => err);
+  }
+
+  function createThumbnail(game) {
+    new CarouselThumbnailView(game, document.querySelector('.carousel__thumbnails'));
   }
 
   function getElements() {
@@ -21,7 +29,7 @@ window.onload = () => {
       first: elements[0],
       last: elements[elements.length -1],
       parent: elements[0].parentNode
-    }
+    };
   }
 
   document.querySelectorAll('.button').forEach((button) => {
@@ -33,7 +41,6 @@ window.onload = () => {
       }
       parent.removeChild(last);
       parent.insertBefore(last, first);
-
-    })
-  })
+    });
+  });
 }
